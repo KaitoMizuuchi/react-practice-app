49bbd4db018276d9d62871a048149755624e9a65
KaitoMizuuchi <kaito.mizuchi@g.dreamcareer.co.jp>
2024-11-14 18:26:36 +0900

    ルーティング設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./assets/styles/common.scss";
 import Header from "./layout/Header";
+import Router from "./Router";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <main className="l-main">メインコンテンツ</main>
+      <main className="l-main">
+        <Router />
+      </main>
     </div>
   );
 }
